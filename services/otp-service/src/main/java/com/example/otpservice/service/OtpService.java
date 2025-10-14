f23fbf166868a17e0cea91c4e900a544f7b00e3f
Z <johncuong1522@gmail.com>
2025-10-15 02:56:40 +0700

    update otp-service

--- services/otp-service/src/main/java/com/example/otpservice/service/OtpService.java
+++ services/otp-service/src/main/java/com/example/otpservice/service/OtpService.java
@@ -4,9 +4,11 @@ import com.example.otpservice.model.OTP;
 import com.example.otpservice.repository.OtpRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 
@@ -15,9 +17,10 @@ public class OtpService {
 
     @Autowired
     private OtpRepository otpRepository;
+//    @Autowired
+//    private EmailService emailService;
     @Autowired
-    private EmailService emailService;
-    
+    private RestTemplate restTemplate;
     private final int EXPIRATION_MINUTES = 2;
 
     public String generateOtp(Integer userId, String purpose, String email) {
@@ -28,16 +31,30 @@ public class OtpService {
         otp.setCode(code);
         otp.setPurpose(purpose);
         otp.setExpiresAt(LocalDateTime.now().plusMinutes(EXPIRATION_MINUTES));
+        sendOtpEmail(email,code);
 
         otpRepository.save(otp);
-
-        if(email != null){
-            emailService.sendOtpEmail(email, code);
-        }
+//        if(email != null){
+//            emailService.sendOtpEmail(email, code);
+//        }
         
         return code;
     }
+    private void sendOtpEmail(String toEmail, String otpCode) {
+        String url = "http://localhost:8088/notifications/send-otp";
 
+        Map<String, String> body = Map.of(
+                "toEmail", toEmail,
+                "otpCode", otpCode
+        );
+
+        try {
+            restTemplate.postForObject(url, body, String.class);
+            System.out.println("✅ OTP email sent successfully to " + toEmail);
+        } catch (Exception e) {
+            System.err.println("❌ Failed to send OTP email: " + e.getMessage());
+        }
+    }
     public boolean verifyOtp(Integer userId, String purpose, String code) {
         Optional<OTP> otpOpt = otpRepository.findTopByUserIdAndPurposeOrderByCreatedAtDesc(userId, purpose);
 
